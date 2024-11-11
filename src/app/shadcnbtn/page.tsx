67723f947fc94226a5bc42169f1aa18900d47eea
imran-wealcoderdev <imran.wealcoder@gmail.com>
2024-11-11 15:25:39 +0600

    feat: add a shadcn button

---
+++ src/app/shadcnbtn/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const ShadcnBtn = () => {
+    return (
+        <main className="h-full flex justify-center items-center flex-col space-y-4">
+            <div>Shadcn Page</div>
+            <Button variant="mine" size="sm">Shadcn Button</Button>
+        </main>
+    )
+}
+
+export default ShadcnBtn 
